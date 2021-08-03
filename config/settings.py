c0137ad7e525361505215245c087d54abf6bf113
st17109hr <st17109hr@gm.ibaraki-ct.ac.jp>
2021-08-03 18:22:15 +0900

    auto commit

--- config/settings.py
+++ config/settings.py
@@ -117,7 +117,7 @@ USE_L10N = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/heroku_static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 STATIC_ROOT = os.path.join(BASE_DIR, 'heroku_static')
 
